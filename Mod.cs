8498f1caad43f114cc2e542867c07c454cea4183
BrokenSt0ne <105749252+BrokenSt0ne@users.noreply.github.com>
2025-01-27 12:38:18 +0000

    Added source code
    make ur own damn project jigsaw!!!!!

---
+++ Mod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BepInEx;
+using UnityEngine;
+
+namespace NoMoreOVR
+{
+    [BepInPlugin("BrokenStone.NoMoreOVR", "NoMoreOVR", "1.0.0")]
+    public class Mod : BaseUnityPlugin
+    {
+        bool ran;
+        bool disabled = false;
+
+        void Update()
+        {
+            if (GorillaTagger.Instance != null && !ran)
+            {
+                ran = true;
+            }
+
+            if(ran && !disabled)
+            {
+                foreach (OVRManager instances in GameObject.FindObjectsOfType<OVRManager>())
+                {
+                    instances.enabled = false;
+                    disabled = true;
+                    Debug.Log("[NoMoreOVR] Disabled OVRManager.");
+                }
+            }
+        }
+    }
+}
